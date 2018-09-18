93aa266c5df158eac4ede08633f9af9b9f053c28
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-18 10:05:33 +0200

    Add project files.

---
+++ Randomizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var raffle = new Raffle();
+            Console.WriteLine("Reading...");
+            raffle.ReadDataFile("Data.txt");
+            foreach (var participant in raffle.Participants)
+            {
+                Console.WriteLine(participant);
+            }
+            Console.WriteLine($"Count: {raffle.CountOfParticipants}");
+            Console.WriteLine("Press any key to pick a winner!");
+            Console.ReadKey();
+            raffle.PickWinner();
+            Console.WriteLine($"Winner: {raffle.Winner}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Randomizer/Raffle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    public class Raffle
+    {
+        private Random randomGenerator = new Random();
+
+        private List<string> participants = new List<string>();
+
+        public int CountOfParticipants
+        {
+            get
+            {
+                return participants.Count;
+            }
+        }
+
+        public IReadOnlyList<string> Participants
+        {
+            get
+            {
+                return participants;
+            }
+        }
+
+        public string Winner { get; private set; }
+
+        public void ReadDataFile(string filename)
+        {
+            participants.Clear();
+            using (var reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    participants.Add(line);
+                }
+            }
+        }
+
+        public void PickWinner()
+        {
+            var index = randomGenerator.Next(CountOfParticipants);
+            Winner = participants[index];
+        }
+    }
+}
